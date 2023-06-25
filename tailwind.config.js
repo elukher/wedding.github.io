0d4ece560a544acbef2a8d99b07735de767c7a72
elukher <luka.herceg91@gmail.com>
2023-06-25 10:37:21 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","src/script.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dancing' : ['Dancing Script'],
+        'kaushan' : ['Kaushan Script'] ,
+        'indie' : ['Indie Flower'],
+        'merrie' : ['Merriweather']
+    }
+    },
+  },
+  plugins: [],
+}
+
